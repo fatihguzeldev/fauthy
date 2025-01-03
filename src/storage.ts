842edfcc605ac7ad4f63eef457a7d43aec740cf9
fatih guzel <fatihguzeldev@gmail.com>
2025-01-04 00:53:03 +0300

    fauthy: `fauthy init` implemented.

--- src/storage.ts
+++ src/storage.ts
@@ -1,15 +1,15 @@
-import fs from 'fs';
-import path from 'path';
-import { Device } from './types/device';
+import fs from "fs";
+import path from "path";
+import { Device } from "./types/device";
 
-const dataFilePath = path.resolve(__dirname, 'data.json');
+const dataFilePath = path.resolve(process.cwd(), "data.json");
 
 export const initDataFile = (): void => {
   if (fs.existsSync(dataFilePath)) {
-    console.log('data.json already exists.');
+    console.log("data.json already exists.");
   } else {
     fs.writeFileSync(dataFilePath, JSON.stringify([]));
-    console.log('data.json has been created.');
+    console.log("data.json has been created.");
   }
 };
 
@@ -17,7 +17,7 @@ export const readData = (): Device[] => {
   if (!fs.existsSync(dataFilePath)) {
     return [];
   }
-  const data = fs.readFileSync(dataFilePath, 'utf-8');
+  const data = fs.readFileSync(dataFilePath, "utf-8");
   return JSON.parse(data);
 };
 
